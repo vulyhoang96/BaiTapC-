263f1821296cc4a0f502fb0105114d6d6a535cc6
= <=>
2020-11-10 23:11:53 +0700

    first commit

---
+++ Baitap1.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	int n;
+	cout << "Nhap n: ";
+	cin >> n;
+	int sum = 0;
+	for (int i = 1; i <= n; i++)
+	{
+		if(i % 2 != 0)
+		sum += i * i;
+	}
+	cout << endl << "Tong binh phuong so le la : " << sum << endl;
+	return 0;
+}
